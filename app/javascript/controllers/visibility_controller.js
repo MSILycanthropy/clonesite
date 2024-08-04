dd497b10b94c07ea9b17556bb019dacd549ab2f5
Ethan Kircher <ethanmichaelk@gmail.com>
2024-08-04 15:20:02 -0500

    add alternate to visibility

--- app/javascript/controllers/visibility_controller.js
+++ app/javascript/controllers/visibility_controller.js
@@ -2,8 +2,8 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="visibility"
 export default class extends Controller {
-  static values = { hidden: { type: Boolean, default: true } };
-  static targets = ["container"];
+  static values = { hidden: { type: Boolean, default: true }, alternateHidden: Boolean };
+  static targets = ["container", "alternate"];
 
   show() {
     this.hiddenValue = false;
@@ -13,7 +13,21 @@ export default class extends Controller {
     this.hiddenValue = true;
   }
 
+  showAlternate() {
+    this.alternateHiddenValue = false;
+  }
+
+  hideAlternate() {
+    this.alternateHiddenValue = true;
+  }
+
   hiddenValueChanged(value) {
     this.containerTargets.forEach((container) => (container.hidden = value));
+  }
+
+  alternateHiddenValueChanged(value) {
+    console.log(value);
+
+    this.alternateTargets.forEach((alternate) => (alternate.hidden = value));
   }
 }
